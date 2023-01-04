dba512bf1c96b93881e629ac6e851bf5a8b3ea9f
Andrés Pardo Rodríguez <andres@Andress-MacBook-Pro.local>
2023-01-04 12:13:21 +0100

    fetch random user + change name through input

--- addons-ts-lab-reactBasics-main/random-user/src/App.tsx
+++ addons-ts-lab-reactBasics-main/random-user/src/App.tsx
@@ -1,30 +1,39 @@
-import React, {FC, createContext} from 'react';
+import React, {FC, createContext, useEffect, useState} from 'react';
 import './App.css';
-import { Person, HairColor } from './components/Person';
-
-interface AppContextInterface {
-  name: string;
-  age: number;
-  country: string
-}
-const AppContext = createContext<AppContextInterface | null>(null);
-
+import { User } from './components/User';
 
 const App: FC = () => {
 
-  const contextValue: AppContextInterface = {
-    name: 'Pecas',
-    age: 4,
-    country: 'England'
-  }
+  const [error, setError] = useState<string | null >(null);
+  const [name, setName] = useState<string>('');
+  const [age, setAge] = useState<number>(0);
+  const [address, setAddress] = useState<string>('');
+  const [gender, setGender] = useState<string>('');
 
+  
+  useEffect(()=>{
+    fetch('https://randomuser.me/api/')
+    .then(response=> response.json())
+    .then(res => {
+      setName(`${res.results[0].name.title} ${res.results[0].name.first} ${res.results[0].name.last}`);
+      setAge(res.results[0].registered.age);
+      setGender(res.results[0].gender);
+      setAddress(`${res.results[0].location.city}, ${res.results[0].location.country} - ${res.results[0].location.street.name} ${res.results[0].location.street.number} - postcode: ${res.results[0].location.postcode}`);
+      console.log(res.results[0]);
+      
+  })
+    .catch(err => setError(err))
+  },[])
+
+  console.log(name, 'here is name');
+  
   return (
-    <AppContext.Provider value={contextValue}>
     <div className="App">
-      <Person name='Andres' age= {20} email ='apardor@gmail.com' hairColor={HairColor.Pink} />
+      <User  name={name} setName={setName} age={age} gender={gender} adress={address}/>
     </div>
-    </AppContext.Provider>
   );
 }
 
 export default App;
+
+
